4972cb08ec5c93c1080789715bcc4a5ed4d0a168
fabidor <felixabidor@gmail.com>
2022-05-25 15:23:06 -0500

    Add basic hover functionality to a 16x16 grid Additionally, made the div boxes hold their own without a text backing.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let sketchBoard = document.querySelector(".sketchboard");
-for(let i = 0; i < 4; i++){
+for(let i = 0; i < 16; i++){
     const sketchColumn=document.createElement('div');
     sketchColumn.classList.add('sketchColumn')
     //sketchColumn.textContent=`I am Column ${i}`
     sketchBoard.appendChild(sketchColumn);
-    for(let j = 0; j < 4; j++){
+    for(let j = 0; j < 16; j++){
         const sketchRow = document.createElement('div');
         sketchRow.classList.add('sketchRow');
-        sketchRow.textContent=`I am Row ${j}`;
+        //sketchRow.textContent=`I am Row ${j}`;
         sketchColumn.appendChild(sketchRow);
     }
 }
+
+let squares = document.querySelectorAll('.sketchRow');
+squares.forEach(square => square.addEventListener('mouseover', () => {
+     square.classList.add('activated')
+     setTimeout(()=>square.classList.remove('activated'), 50)
+}));
