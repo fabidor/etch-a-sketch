82267e9d9c70d301dfb0e137bb28115cfc15f262
fabidor <felixabidor@gmail.com>
2022-05-26 14:49:06 -0500

    Add mouseup event listener to deactivate draw Additionally, added functionality for the user to use a slider to change the size of the board.

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
 let sketchBoard = document.querySelector(".sketchboard");
-let squareNum = prompt("Who Many Squares??", 0);
-for(let i = 0; i < squareNum; i++){
-    const sketchColumn=document.createElement('span');
-    sketchColumn.classList.add('sketchColumn')
-    //sketchColumn.textContent=`I am Column ${i}`
-    sketchBoard.appendChild(sketchColumn);
-    for(let j = 0; j < squareNum; j++){
-        const sketchRow = document.createElement('span');
-        sketchRow.classList.add('sketchRow');
-        //sketchRow.textContent=`I am Row ${j}`;
-        sketchColumn.appendChild(sketchRow);
+//let squareNum = prompt("Who Many Squares??", 0);
+let squares;
+
+
+function clearBoard(){
+    while(sketchBoard.firstChild){
+        sketchBoard.removeChild(sketchBoard.firstChild)
     }
 }
+function setBoard(squareNum){
+    clearBoard();
+    for(let i = 0; i < squareNum; i++){
+        const sketchColumn=document.createElement('span');
+        sketchColumn.classList.add('sketchColumn')
+        //sketchColumn.textContent=`I am Column ${i}`
+        sketchBoard.appendChild(sketchColumn);
+        for(let j = 0; j < squareNum; j++){
+            const sketchRow = document.createElement('span');
+            sketchRow.classList.add('sketchRow');
+            //sketchRow.textContent=`I am Row ${j}`;
+            sketchColumn.appendChild(sketchRow);
+        }
+    }
+    squares=document.querySelectorAll('.sketchRow');
+}
+setBoard(50);
+ 
 
-let squares = document.querySelectorAll('.sketchRow');
-squares.forEach(square => square.addEventListener('mouseover', () => {
-     square.classList.add('activated')
-     setTimeout(()=>square.classList.remove('activated'), 50)
-}));
+
+function draw(){
+    this.classList.add('activated');
+}
+
+sketchBoard.addEventListener('mousedown', () =>{
+    console.log(true);
+    squares.forEach(square => square.addEventListener('mouseover', draw))
+    } 
+);
+
+ sketchBoard.addEventListener('mouseup', () => {
+     squares.forEach(square => {
+         square.removeEventListener('mouseover', draw);
+     })
+ })
+
+
+
